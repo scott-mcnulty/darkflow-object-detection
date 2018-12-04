2f8467c55f43698038d1a660030f044c758727c3
Scott McNulty <mcnultyscott10@gmail.com>
2018-12-03 19:53:08 -0600

    Remove old funciton from predict to get camera images

--- inference-computer/predict.py
+++ inference-computer/predict.py
@@ -31,22 +31,3 @@ class Predictor():
         logging.debug('predict')
         prediction = self.tfnet.return_predict(detection_image_cv2)
         return prediction
-
-    def predict_images_camera_server(self, draw_box=False):
-        """Gets images and saves them."""
-
-        while True:
-
-            logging.info('Getting image from camera server')
-            detection_image_cv2 = self.get_image_camera_server(config.local_image_path)
-            prediction = self.tfnet.return_predict(detection_image_cv2)
-            
-            for detection in prediction:
-                if detection['lable'] in config.labels:
-                    self.save_image(config.base_image_path, detection['label'], detection_image_cv2, True)
-
-            if draw_box:
-                drawn_on_image = self.draw_boxes(prediction, detection_image)
-                self.save_image(config.base_labeled_image_path, '', detection_image)
-
-            time.sleep(4)
