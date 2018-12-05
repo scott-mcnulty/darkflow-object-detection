d255a26a65e125f44f0b20d4440c58e60dd72fc8
Scott McNulty <mcnultyscott10@gmail.com>
2018-12-04 19:08:27 -0600

    Add video feed to camera

--- camera-server/app.py
+++ camera-server/app.py
@@ -4,21 +4,42 @@ import os
 
 
 from flask import Flask, render_template, Response
-from camera_opencv import Camera
+from camera_opencv import Camera, VideoCamera
 
 app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 def gen2(camera):
     """Returns a single image frame"""
     frame = camera.get_frame()
     yield frame
 
-@app.route('/image.jpg')
+
+@app.route('/image')
 def image():
-    """
-    Returns a single current image for the webcam.
-    """
+    """Returns a single current image for the webcam."""
     return Response(gen2(Camera()), mimetype='image/jpeg')
 
+
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+@app.route('/video_feed')
+def video_feed():
+    """Gets live feed video from camera"""
+    return Response(gen(VideoCamera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', threaded=True)
+    # app.run(host='0.0.0.0', debug=True)
+    app.run(host='0.0.0.0', threaded=True, debug=True)
